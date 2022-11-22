b72a8108150e7f555946fd765aab2d6e12e8ed35
Anand Mahesh <manand881@gmail.com>
2022-11-22 08:02:07 +0530

    minor fixes

--- modules/setup_instance.py
+++ modules/setup_instance.py
@@ -1,11 +1,9 @@
-from os import path, mkdir, getcwd,listdir
+from os import getcwd, mkdir, path
 
 dirs = ["logs"]
-print(getcwd())
 for dir in dirs:
-    print(path.exists(dir))
     if not path.exists(dir):
         mkdir(path.join(getcwd(), dir))
-print(listdir())
+
 with open('logs/app.log', 'w+') as fp:
     fp.close()
